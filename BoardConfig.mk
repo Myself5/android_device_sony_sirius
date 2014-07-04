5cb21ab4f01ea751df4ac2d7a2ac21879f83a389
xNUTx <nut@fun-industries.nl>
2014-07-04 12:27:13 +0200

    Ripping out the kernel building... part 4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/sirius/bluetooth
 
 # Kernel properties
 #TARGET_KERNEL_CONFIG := cm_shinano_sirius_defconfig
-TARGET_PREBUILT_KERNEL := device/sony/sirius/prebuilt/boguskernel
+TARGET_PREBUILT_KERNEL := device/sony/sirius/kernel
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
