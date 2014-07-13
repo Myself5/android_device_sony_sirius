da3afa25fae7c5ee81af4714b2f2b76a21e41e1f
Myself5 <thchod@gmail.com>
2014-07-13 20:49:27 +0200

    Carbonify
    Change-Id: Ic6d94d7636a8c67a82067c50157897920f2523c0

---
+++ carbon.mk
@@ -0,0 +1,9 @@
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+$(call inherit-product, device/sony/sirius/full_sirius.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503 BUILD_FINGERPRINT=Sony/D6503/D6503:4.4.2/17.1.1.A.0.402/bf53dw:user/release-keys PRIVATE_BUILD_DESC="D6503-user 4.4.2 17.1.1.A.0.402 bf53dw release-keys"
+
+PRODUCT_NAME := carbon_sirius
+PRODUCT_DEVICE := sirius
