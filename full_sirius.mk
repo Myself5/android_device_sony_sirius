a5c95f12f5398339c3bb9f711c39623a784a69a1
Myself5 <thchod@gmail.com>
2014-07-18 18:11:54 +0200

    Add QuickBoot
    Change-Id: Ib2a46f81b299870394f5bf795e8e3da851bd826c

--- full_sirius.mk
+++ full_sirius.mk
@@ -16,9 +16,10 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Torch
+# Extra Packages
 PRODUCT_PACKAGES := \
     Torch
+    QuickBoot
  
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
