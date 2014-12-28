400308b41f6f1067a9b26b36ea0b2a3ed46cbc9d
Myself5 <myself5@carbonrom.org>
2014-12-28 15:17:54 +0100

    Omnify Sirius

--- omni_sirius.mk
+++ omni_sirius.mk
@@ -1,12 +1,12 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit Omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503 BUILD_FINGERPRINT=Sony/D6503/D6503:4.4.2/17.1.2.A.0.314/xf5vdw:user/release-keys PRIVATE_BUILD_DESC="D6503-user 4.4.2 17.1.2.A.0.314 xf5vdw release-keys"
 
-PRODUCT_NAME := cm_sirius
+PRODUCT_NAME := omni_sirius
 PRODUCT_DEVICE := sirius
